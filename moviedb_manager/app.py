9f72340fe94dd2c3ac5e5b1f8354f8d680384007
Anton Lu <anton96lu@gmail.com>
2026-03-20 18:35:22 +0100

    refactor: apply type hints and fix linting issues

--- moviedb_manager/app.py
+++ moviedb_manager/app.py
@@ -1,132 +1,161 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 
-from argparse import ArgumentParser
-import re
-import errno
 import os
-import sys
-import shutil
+import typing
+from argparse import ArgumentParser
 from time import sleep
-from glob import glob, escape
-from multiprocessing import Process
 
-from flask import Flask, render_template, request
+import tvdbsimple as tvdb
 import yaml
+
+# from process import process
+from celery import Celery
+from flask import Flask, render_template, request
 from qbittorrentapi import Client
 from tmdbv3api import Movie, TMDb
-import tvdbsimple as tvdb
 
-from filemanagement import Media
-# from process import process
+from .filemanagement import Media, MediaInit
 
-from celery import Celery
-
-
-parser = ArgumentParser()
-
-parser.add_argument("config", help="Path to config YAML file")
-
-args = parser.parse_args()
-
-global config
-with open(args.config, "r") as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
-
-tmdb = TMDb()
-tmdb.api_key = config['apikeys']['tmdb']
-movie = Movie()
-tvdb.KEYS.API_KEY = config['apikeys']['tvdb']
-
-q = config["qbittorrent"]
-d = config["directories"]
-
-client = Client(host=q["host"] + ":" + str(q["port"]),
-                username=q["user"], password=q["password"])
-
-print("Using qBittorrent Version: %s" % client.app_version())
-
-config["qbittorrent"] = client
-config["tmdb"] = movie
-config["tvdb"] = tvdb
-
+config: dict[str, typing.Any] = {}
 
 app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = config["celery"]["celery_url"]
-app.config['CELERY_RESULT_BACKEND'] = config["celery"]["celery_result"]
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
+celery = Celery(app.name)
+tmdb = TMDb()
+movie = Movie()
+
+
+def load_config(config_path: str) -> dict[str, typing.Any]:
+    with open(config_path, encoding="utf-8") as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+
+def init_services(app_config: dict[str, typing.Any]) -> None:
+    config.clear()
+    config.update(app_config)
+
+    tmdb.api_key = config["apikeys"]["tmdb"]
+    tvdb.KEYS.API_KEY = config["apikeys"]["tvdb"]
+
+    q = config["qbittorrent"]
+    client = Client(
+        host=q["host"] + ":" + str(q["port"]),
+        username=q["user"],
+        password=q["password"],
+    )
+
+    print(f"Using qBittorrent Version: {client.app_version()}")
+
+    config["qbittorrent"] = client
+    config["tmdb"] = movie
+    config["tvdb"] = tvdb
+
+    app.config["CELERY_BROKER_URL"] = config["celery"]["celery_url"]
+    app.config["CELERY_RESULT_BACKEND"] = config["celery"]["celery_result"]
+    celery.conf.update(app.config)
 
 
 @celery.task
-def process(magnet_uri: str, typ: str, config):
-    client = config["qbittorrent"]
-    tmdb = config["tmdb"]
-    tvdb = config["tvdb"]
+def process(
+    magnet_uri: str, typ: str, app_config: dict[str, typing.Any] | None = None
+) -> bool:
+    if app_config is None:
+        app_config = config
 
-    d = config["directories"]
+    client = app_config["qbittorrent"]
+    tmdb = app_config["tmdb"]
+    tvdb = app_config["tvdb"]
 
-    res= client.torrents_add(
-        urls=[magnet_uri], save_path=os.path.join(d['remote'], d["download"]), is_paused=True)
+    d = app_config["directories"]
+
+    res = client.torrents_add(
+        urls=[magnet_uri],
+        save_path=os.path.join(d["remote"], d["download"]),
+        is_paused=True,
+    )
 
     if res != "Ok.":
-        print("Torrent add failed: {}".format(res))
+        print(f"Torrent add failed: {res}")
 
-    torrent_list= client.torrents_info(status_filter='paused')
+    torrent_list = client.torrents_info(status_filter="paused")
 
-    active_torrents= {}
+    active_torrents = {}
 
-    torrent= None
+    torrent = None
 
     for torrent in torrent_list:
         if torrent["magnet_uri"] not in active_torrents:
-            obj= {
-                'name': torrent['name'],
-                'magnet_uri': torrent['magnet_uri'],
-                'hash': torrent['hash']
+            obj = {
+                "name": torrent["name"],
+                "magnet_uri": torrent["magnet_uri"],
+                "hash": torrent["hash"],
             }
-            client.torrents_resume(obj['hash'])
-            active_torrents[obj['magnet_uri']]= obj
+            client.torrents_resume(obj["hash"])
+            active_torrents[obj["magnet_uri"]] = obj
             if torrent["magnet_uri"] == magnet_uri:
-                torrent= obj
+                torrent = obj
 
     print("Torrent in progress")
     while client.torrents_info(hash=torrent["hash"])[0]["state"] != "uploading":
         sleep(1)
 
     # this assumes that the 0th file is in the root directory of the download
-    torrent['data_root']= os.path.split(
-        client.torrents_files(hash=torrent['hash'])[0]["name"])[0]
-    client.torrents_delete(delete_files=False, hashes=torrent['hash'])
+    torrent["data_root"] = os.path.split(
+        client.torrents_files(hash=torrent["hash"])[0]["name"]
+    )[0]
+    client.torrents_delete(delete_files=False, hashes=torrent["hash"])
 
     print("Torrent complete")
 
     # TODO: look for media files inside local dir
     # TODO: move files asynchronously
 
-    media= Media(config, torrent["name"], torrent["magnet_uri"],
-                torrent["data_root"], tmdb, tvdb, typ=typ)
+    media = Media(
+        app_config,
+        MediaInit(
+            name=torrent["name"],
+            magnet_uri=torrent["magnet_uri"],
+            data_root=torrent["data_root"],
+            tmdb=tmdb,
+            tvdb=tvdb,
+            typ=typ,
+        ),
+    )
 
     media.process()
 
     return True
 
+
 @app.route("/mediamanager")
-def index():
+def index() -> str:
     return render_template("index.html")
 
 
 @app.route("/mediamanager/datahandler", methods=["POST"])
-def handle_data():
-    magnet_uri= request.form.get("magnet_uri")
-    typ= request.form.get("type_selector")
+def handle_data() -> str | tuple[str, int]:
+    magnet_uri = request.form.get("magnet_uri")
+    typ = request.form.get("type_selector")
 
-    print("Received request with magnet: {}".format(magnet_uri))
+    if magnet_uri is None or typ is None:
+        return "Missing request data", 400
 
-    task= process(magnet_uri, typ, config).delay()
+    print(f"Received request with magnet: {magnet_uri}")
 
-    return "Success! Added magnet link {}".format(magnet_uri)
+    process.delay(magnet_uri, typ, config)
+
+    return f"Success! Added magnet link {magnet_uri}"
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument("config", help="Path to config YAML file")
+    args = parser.parse_args()
+
+    init_services(load_config(args.config))
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    main()
--- moviedb_manager/filemanagement.py
+++ moviedb_manager/filemanagement.py
@@ -1,54 +1,59 @@
-import re
+from __future__ import annotations
+
 import errno
 import os
+import re
 import shutil
-from glob import glob, escape
-from requests.exceptions import HTTPError
+import typing
+from dataclasses import dataclass
+from glob import escape, glob
 
 
-# TODO: logger
+@dataclass(slots=True)
+class MediaInit:
+    name: str
+    magnet_uri: str
+    data_root: str
+    tmdb: typing.Any = None
+    tvdb: typing.Any = None
+    typ: str = "movie"
+
 
 class Media:
-    def __init__(self, config, name, magnet_uri, data_root, tmdb=None, tvdb=None, typ='movie'):
-        super().__init__()
-        self.directories = config['directories']
-        self.tmdb = tmdb
-        self.tvdb = tvdb
-        self.typ = typ
-        self.name = name
-        self.magnet_uri = magnet_uri
-        self.mediafiles = []
-        self.data_root = data_root
-        self.type = typ
+    def __init__(self, config: dict[str, typing.Any], init: MediaInit) -> None:
+        self.directories = config["directories"]
+        self.tmdb = init.tmdb
+        self.tvdb = init.tvdb
+        self.typ = init.typ
+        self.name = init.name
+        self.magnet_uri = init.magnet_uri
+        self.data_root = init.data_root
+        self.type = init.typ
 
-        self.media_extensions = config['misc']['mediaextensions']
-        self.subtitle_extensions = config['misc']['subtitleextensions']
+        self.mediafiles: list[MediaFile] = []
+        self.media_extensions = config["misc"]["mediaextensions"]
+        self.subtitle_extensions = config["misc"]["subtitleextensions"]
 
         self.__initialise_variables()
 
-    def __initialise_variables(self):
-            # .2019.1080p.XXXXX , .2019.XXXXX , .1080p.XXXXX
-        # 2019.1080p.XXXXX , 2019.XXXXX , 1080p.XXXXX
+    def __initialise_variables(self) -> None:
         self.filename_regex = [
             re.compile(r"([\.\+]\d{4}[\.\+]\d{3,4}p[\.\+]).+"),
-            re.compile(r"([\.\+]\d{4}[\.\+]).*"), 
+            re.compile(r"([\.\+]\d{4}[\.\+]).*"),
             re.compile(r"[\.\+]\d{3,4}p"),
-            re.compile(r"(\d{4}[\.\+]\d{3,4}p[\.\+]).+"), 
-            re.compile(r"(\d{4}[\.\+]).*"), 
-            re.compile(r"[\.\+]\d{3,4}p")
+            re.compile(r"(\d{4}[\.\+]\d{3,4}p[\.\+]).+"),
+            re.compile(r"(\d{4}[\.\+]).*"),
+            re.compile(r"[\.\+]\d{3,4}p"),
         ]
-
-        self.year_regex = re.compile(r'(?:19|20)\d{2}')
+        self.year_regex = re.compile(r"(?:19|20)\d{2}")
         self.episode_regex = re.compile(r"S\d{2}E\d{2}")
-
         self.dotremove_regex = re.compile(r"[\.\+]")
-
         self.moved_to_library = False
 
-    def process(self):
-
-        # find file
-        self.data_path = os.path.join(self.directories["local"], self.directories["download"], self.data_root)
+    def process(self) -> None:
+        self.data_path = os.path.join(
+            self.directories["local"], self.directories["download"], self.data_root
+        )
 
         if not os.path.exists(self.data_path):
             print("Cannot find downloaded files in " + self.name)
@@ -60,91 +65,73 @@ class Media:
                 candidate = self.data_path + ext
                 if os.path.isfile(candidate):
                     self.mediafiles = [
-                        MediaFile(os.path.basename(candidate), candidate, self.type)]
+                        MediaFile(os.path.basename(candidate), candidate, typ=self.type)
+                    ]
 
         if self.mediafiles == []:
             print("No media files found")
-            # TODO: aise some exception
 
         self.__find_name(self.mediafiles)
         self.__move_to_library()
 
         shutil.rmtree(self.data_path)
 
-
-    def __find_mediafiles(self, data_path : str, files=[]):
+    def __find_mediafiles(
+        self, data_path: str, files: list[MediaFile] | None = None
+    ) -> list[MediaFile]:
+        if files is None:
+            files = []
 
         for ext in self.media_extensions:
             found = glob(os.path.join(escape(data_path), "*." + ext))
-            for f in found:
-                files.append(MediaFile(os.path.basename(f), f, typ='movie'))
-                # TODO: find subtitles
+            for path in found:
+                files.append(MediaFile(os.path.basename(path), path, typ="movie"))
 
         if files == []:
             for subfolder in os.listdir(data_path):
-                self.__find_mediafiles(subfolder, files)
+                subpath = os.path.join(data_path, subfolder)
+                if os.path.isdir(subpath):
+                    self.__find_mediafiles(subpath, files)
 
         return files
 
-
-    def __process_filename(self, filename : str):
-        """
-        Extracts useful information from the filename of a media file like real name,
-        episode data.
-
-        TODO: 
-        Parameters
-        ----------
-        filename : str
-            The original filename of a media, eg. The.Mandalorian.S01E01.x264.WEBRIP.mkv
-
-        Returns
-        -------
-        dict
-            A dictionary containing keys 'name', 'year', 'season', 'episode', where applicable. 
-        """
-        for r in self.filename_regex:
-            res = r.search(filename)  # strip filename
-
+    def __process_filename(self, filename: str) -> dict[str, typing.Any]:
+        res = None
+        for regex in self.filename_regex:
+            res = regex.search(filename)
             if res:
                 break
 
-        stripped_filename = filename.split(res.group(0))[0]
-
-        out = {"name": self.dotremove_regex.sub(" ", stripped_filename)}
+        stripped_filename = filename.split(res.group(0))[0] if res else filename
+        out: dict[str, typing.Any] = {
+            "name": self.dotremove_regex.sub(" ", stripped_filename)
+        }
 
         try:
-            year = self.year_regex.findall(filename)[-1]
-            out["year"] = year
-        except IndexError: 
+            out["year"] = self.year_regex.findall(filename)[-1]
+        except IndexError:
             out["year"] = ""
             print("could not find year data")
 
         if self.type == "tv":
-            try: 
+            try:
                 episode_data = self.episode_regex.findall(filename)[-1]
-
                 out["season"] = int(episode_data[1:3])
                 out["episode"] = int(episode_data[4:6])
-
                 out["name"] = out["name"].split(episode_data)[0].rstrip()
             except IndexError:
                 print("could not find episode data")
 
         return out
 
+    def __find_name(self, mediafiles: list[MediaFile]) -> None:
+        for mediafile in mediafiles:
+            name_data = self.__process_filename(mediafile.filename)
+            year = name_data["year"] or ""
 
-    def __find_name(self, mediafiles : list):
-
-        for f in mediafiles:
-            name_data = self.__process_filename(f.filename)
-
-            year = name_data["year"]
-            year = year if year != "" else ""
-
-            if self.type == 'movie':
+            if self.type == "movie":
                 res = self.tmdb.search(name_data["name"])
-            elif self.type == 'tv':
+            else:
                 search = self.tvdb.Search()
                 res = search.series(name_data["name"])[0]
                 tv_id = res["id"]
@@ -153,156 +140,154 @@ class Media:
                 print("No results found")
 
             if self.type == "movie":
+                metadata = None
                 for item in res:
                     if item.release_date.startswith(year):
-                        title = item.title
                         metadata = item
                         break
 
-                title = "{} ({})".format(metadata.original_title, metadata.release_date.split('-')[0])
+                if metadata is None:
+                    metadata = res[0]
 
-                f.metadata = metadata
-            elif self.type == "tv":
-                #TODO: add year check
-
+                title = (
+                    f"{metadata.original_title} ({metadata.release_date.split('-')[0]})"
+                )
+                mediafile.metadata = metadata
+            else:
                 season = name_data["season"]
                 episode = name_data["episode"]
 
-                episode_no= "S{:02d}E{:02d}".format(season, episode)
+                episode_no = f"S{season:02d}E{episode:02d}"
                 show = self.tvdb.Series(tv_id)
-
                 show.Episodes.update_filters(airedSeason=season, airedEpisode=episode)
                 episode_name = show.Episodes.all()[0]["episodeName"]
                 showname = show.info()["seriesName"]
 
-                title = "{} - {} - {}".format(showname, episode_no, episode_name)
+                title = f"{showname} - {episode_no} - {episode_name}"
+                mediafile.metadata = show
+                mediafile.set_episode_data(season, episode)
 
-                f.metadata = show
-                f.set_episode_data(season, episode)
+            title = re.compile(r"\:").sub("", title)
+            mediafile.set_realname(title)
 
-            regex = re.compile(r'\:') # remove illegal characters
-            title = regex.sub('', title)
-
-            f.set_realname(title)
-
-
-    def __move_to_library(self):
+    def __move_to_library(self) -> None:
         if self.moved_to_library:
-            raise EnvironmentError("Already moved to library once!")
+            msg = "Already moved to library once!"
+            raise OSError(msg)
 
         if self.type == "movie":
-            dest = os.path.join(self.directories['local'],
-                        self.directories['movie'])
-        elif self.type == "tv":
-            dest = os.path.join(self.directories['local'],
-                        self.directories['tv'])
+            dest = os.path.join(self.directories["local"], self.directories["movie"])
+        else:
+            dest = os.path.join(self.directories["local"], self.directories["tv"])
 
-        for f in self.mediafiles:
-            f.rename(f.realname)
+        for mediafile in self.mediafiles:
+            mediafile.rename(mediafile.realname or mediafile.filename)
             if self.type == "tv":
-                season, _ = f.episode_data
-                showname = f.metadata.seriesName
-
-                series_dest = os.path.join(dest, showname, "Season {}".format(season))
-                f.move(series_dest)
+                season, _ = mediafile.get_episode_data()
+                showname = mediafile.metadata.seriesName
+                series_dest = os.path.join(dest, showname, f"Season {season}")
+                mediafile.move(series_dest)
             else:
-                f.move(dest)
+                mediafile.move(dest)
 
         self.moved_to_library = True
 
+    def move_to(self, new_dir: str) -> None:
+        for mediafile in self.mediafiles:
+            mediafile.move(new_dir)
 
-    def move_to(self, new_dir):
-        for f in self.mediafiles:
-            f.move(new_dir)
 
-
-def mkdir_p(path):
-    # https://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
+def mkdir_p(path: str) -> None:
     try:
         os.makedirs(path)
-    except OSError as exc:  # Python >2.5
+    except OSError as exc:
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else:
             raise
 
 
-
 class AbstractFile:
-    def __init__(self, filename, absolutepath, children=[], childpath="Subtitles"):
+    def __init__(
+        self,
+        filename: str,
+        absolutepath: str,
+        children: list[AbstractFile] | None = None,
+        childpath: str = "Subtitles",
+    ) -> None:
+        if children is None:
+            children = []
         self.filename = filename
         self.absolutepath = absolutepath
         self.extension = os.path.splitext(os.path.basename(filename))[1]
-        
         self.children = children
         self.childpath = childpath
+        self.season: int | None = None
+        self.episode: int | None = None
 
-        self.season = None
-        self.episode = None
-
-    def move(self, dest):
-        
+    def move(self, dest: str) -> None:
         mkdir_p(dest)
-
         newpath = os.path.join(dest, self.filename + self.extension)
-        
-        os.rename(self.absolutepath, os.path.join(dest, self.filename + self.extension))
+        os.rename(self.absolutepath, newpath)
         self.absolutepath = newpath
 
         for child in self.children:
-            child.move(self, os.path.join(dest, self.childpath))
-    
-    def rename(self, newname, keep_ext=True):
-        
-        if keep_ext:
-            name = newname + self.extension
-        
+            child.move(os.path.join(dest, self.childpath))
+
+    def rename(self, newname: str) -> None:
+        name = newname + self.extension
         dest = os.path.split(self.absolutepath)[0]
-
         newpath = os.path.join(dest, name)
-
         os.rename(self.absolutepath, newpath)
         self.filename = name
         self.absolutepath = newpath
 
         for child in self.children:
-            child.rename(newname, keep_ext=keep_ext)
+            child.rename(newname)
 
-    def set_episode_data(self, season, episode):
+    def set_episode_data(self, season: int, episode: int) -> None:
         self.season = season
         self.episode = episode
 
-    def get_episode_data(self):
+    def get_episode_data(self) -> tuple[int | None, int | None]:
         return self.season, self.episode
-    
-    episode_data = property(get_episode_data, set_episode_data, "Episode data")
-
-
 
 
 class MediaFile(AbstractFile):
-    def __init__(self, filename, absolutepath, realname=None, typ='movie', subtitle_path="Subtitles", **kwargs):
+    def __init__(
+        self,
+        filename: str,
+        absolutepath: str,
+        realname: str | None = None,
+        typ: str = "movie",
+        subtitle_path: str = "Subtitles",
+        **kwargs: typing.Any,
+    ) -> None:
         super().__init__(filename, absolutepath, childpath=subtitle_path)
         self.realname = realname
-        self.metadata = None
+        self.metadata: typing.Any = None
         self.type = typ
-    
-    def set_realname(self, realname):
+
+    def set_realname(self, realname: str) -> None:
         self.realname = realname
-        
+
         for sub in self.children:
             if type(sub) is not SubtitleFile:
-                raise AssertionError(f"{sub} is not of type SubtitleFile")
+                msg = f"{sub} is not of type SubtitleFile"
+                raise AssertionError(msg)
             sub.realname = realname
 
-    def add_subtitle(self, subtitle):
+    def add_subtitle(self, subtitle: SubtitleFile) -> None:
         if type(subtitle) is not SubtitleFile:
-            raise TypeError(f"{subtitle} is not of type SubtitleFile")
+            msg = f"{subtitle} is not of type SubtitleFile"
+            raise TypeError(msg)
 
         self.children.append(subtitle)
 
 
 class SubtitleFile(AbstractFile):
-    def __init__(self, filename, absolutepath, realname=None):
+    def __init__(
+        self, filename: str, absolutepath: str, realname: str | None = None
+    ) -> None:
         super().__init__(filename, absolutepath)
         self.realname = realname
